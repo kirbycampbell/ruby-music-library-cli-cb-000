5769a04fffc61005cafd4eaec6b0f4c09c11177b
Kirby Campbell <jkirbycampbell@gmail.com>
2018-05-16 17:02:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
 
   def initialize(name, artist = nil)
     @name = name
-    self.artist = artist unless artist = nil
+    self.artist = artist unless artist.nil?
   end
 
   def self.all
